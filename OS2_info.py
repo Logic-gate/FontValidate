304a93132725b7c310202a775146e87d4fe7b66d
mad-dev <mad-dev@SYSBASE-212.(none)>
2014-08-23 08:37:52 +0300

    initial commit

---
+++ OS2_info.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Amer Almadani'
+__email__ = 'mad_dev@linuxmail.org'
+
+verion = '0.2'
+
+change_log ='''
+
+2014-08-22 
+
+	* added compare class
+
+'''
+from fontTools import ttLib
+from collections import defaultdict, OrderedDict, Counter
+import argparse
+import ConfigParser
+from os import path
+
+OS2 = [ 'xAvgCharWidth', 
+			'usWeightClass',
+			'usWidthClass',
+			'fsType',
+			'ySubscriptXSize',
+			'ySubscriptYSize',
+			'ySubscriptXOffset',
+			'ySubscriptYOffset',
+			'ySuperscriptXSize',
+			'ySuperscriptYSize',
+			'ySuperscriptXOffset',
+			'ySuperscriptYOffset',
+			'yStrikeoutSize',
+			'yStrikeoutPosition',
+			'sFamilyClass',
+			'ulUnicodeRange1',
+			'ulUnicodeRange2',
+			'ulUnicodeRange3',
+			'ulUnicodeRange4',
+			'achVendID',
+			'fsSelection',
+			'fsFirstCharIndex',
+			'fsLastCharIndex',
+			'sTypoAscender',
+			'sTypoDescender',
+			'sTypoLineGap',
+			'usWinAscent',
+			'usWinDescent'
+			]
+
+class GetMetrics:
+
+	def __init__(self, font):
+		self.font = font
+		self.ttf = ttLib.TTFont(font)
+
+	def metrics(self, cmd):
+		'''metrics(cmd) >> return a command;
+		   self.ttf['OS/2'].{cmd}'''
+		command = []
+
+		for i in OS2:
+			if cmd == i:
+				out = defaultdict(lambda: OS2, {cmd:i})[cmd]
+				fontTools_command = "print '%s =', self.ttf['OS/2'].%s" %(i, out)
+				command.append(fontTools_command)
+
+			elif cmd == 'OS/2':
+				fontTools_command = "print '%s =', self.ttf['OS/2'].%s" %(i, i)
+				command.append(fontTools_command)
+		
+		return OS2, command
+				
+
+	def validate(self, mode, param):
+		'''Valid(mode, param) >> exec the command from GetMetrics.metrics(cmd):
+
+		   mode:
+		   		all - Return all defined properties(param = None)
+		   		singular - Return value of param'''
+
+		if mode == 'all':
+			param = None
+			a = self.metrics('OS/2')[1]
+			for i in a:
+				exec i
+
+		elif mode == 'singular':
+			a = self.metrics(param)[1]
+			for i in a:
+				exec i
+
+
+class Compare:
+
+	def __init__(self, metrics_file):
+		self.metrics_file = metrics_file
+		self.config = ConfigParser.ConfigParser()
+
+	def readMetrics(self, properties):
+		
+		values = []
+		self.config.read(self.metrics_file)
+		section = self.config.sections()
+		if properties is not None:
+			for i in section:
+				values.append(self.config.get(i, properties))
+				pass
+
+
+		return section, values
+
+
+	def duplicates(self, values):
+		'''FIXME if given multi-font metric files and two have an equal value, there is no way to tell which metric file is diff'''
+		remove = list(OrderedDict.fromkeys(values))
+		if len(remove) > 1:
+			print '\033[91m%s\033[0m' %remove
+		else:
+			print '\033[92mValues Are Equal\033[0m\n'
+	
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(prog=__file__, description='Return OS/2 properties of a given font', 
+									 formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+									 epilog='''For font list, seperate the paths with ";"
+									  e.g OS2_info.py -i "/DroidSansArabic_OLD.otf;/DroidSansArabic.otf" -p achVendID''')
+	parser.add_argument('-i', metavar='--input_font', help='Font file', required=False)
+	parser.add_argument('-p', metavar='--os2_property', help='Property as per the OS/2 format', default='all')
+	parser.add_argument('-c', metavar='--compare', help='Take a file name as a param')
+
+	args = parser.parse_args()
+
+	def operation(font):
+		OS = GetMetrics(font)
+		if args.p == 'all':
+			OS.validate(args.p, None)
+			#OS.SetConfig(i)
+		else:
+			OS.validate('singular', args.p)
+
+	if args.i is not None:
+		if ';' in args.i:
+			font_list = args.i.split(';')
+			for i in font_list:
+			
+				print '\n[%s]' %i
+				operation(i)
+		else:
+			operation(args.i)
+	if args.c is not None:
+		if path.isfile(args.c):
+			c = Compare(args.c)
+			print '\033[95mRespective Comparison between\033[0m'
+			for section in c.readMetrics(None)[0]:
+				print '\033[93m%s\033[0m' %section
+			for i in OS2:
+				print '\033[94m%s\033[0m' %i
+				c.duplicates(c.readMetrics(i)[1])
+		else:
+			print args.c, 'was not found'
+
+
--- README.md
+++ README.md
@@ -2,3 +2,172 @@ FontValidate
 ============
 
 OS/2 info
+
+---
+
+```
+python OS2_info.py -i 'Ubuntu-B.ttf;Ubuntu-R.ttf' >> fonts.metrics
+```
+Will generate:
+
+```
+[Ubuntu-B]
+xAvgCharWidth = 652
+usWeightClass = 700
+usWidthClass = 5
+fsType = 0
+ySubscriptXSize = 700
+ySubscriptYSize = 650
+ySubscriptXOffset = 0
+ySubscriptYOffset = 140
+ySuperscriptXSize = 700
+ySuperscriptYSize = 650
+ySuperscriptXOffset = 0
+ySuperscriptYOffset = 477
+yStrikeoutSize = 120
+yStrikeoutPosition = 250
+sFamilyClass = 0
+ulUnicodeRange1 = 3758107391
+ulUnicodeRange2 = 3489701979
+ulUnicodeRange3 = 8
+ulUnicodeRange4 = 0
+achVendID = DAMA
+fsSelection = 32
+fsFirstCharIndex = 0
+fsLastCharIndex = 65276
+sTypoAscender = 776
+sTypoDescender = -188
+sTypoLineGap = 61
+usWinAscent = 1274
+usWinDescent = 551
+
+[Ubuntu-R]
+xAvgCharWidth = 500
+usWeightClass = 700
+usWidthClass = 5
+fsType = 0
+ySubscriptXSize = 700
+ySubscriptYSize = 650
+ySubscriptXOffset = 0
+ySubscriptYOffset = 140
+ySuperscriptXSize = 700
+ySuperscriptYSize = 650
+ySuperscriptXOffset = 0
+ySuperscriptYOffset = 477
+yStrikeoutSize = 50
+yStrikeoutPosition = 250
+sFamilyClass = 0
+ulUnicodeRange1 = 3758097151
+ulUnicodeRange2 = 1342208251
+ulUnicodeRange3 = 0
+ulUnicodeRange4 = 0
+achVendID = DAMA
+fsSelection = 32
+fsFirstCharIndex = 0
+fsLastCharIndex = 65533
+sTypoAscender = 693
+sTypoDescender = -168
+sTypoLineGap = 164
+usWinAscent = 835
+usWinDescent = 259
+```
+---
+```
+python OS2_info.py -c fonts.metrics
+```
+Will produce:
+
+```
+Respective Comparison between
+Ubuntu-BI
+Ubuntu-R
+xAvgCharWidth
+['652', '500']
+usWeightClass
+Values Are Equal
+
+usWidthClass
+Values Are Equal
+
+fsType
+Values Are Equal
+
+ySubscriptXSize
+Values Are Equal
+
+ySubscriptYSize
+Values Are Equal
+
+ySubscriptXOffset
+Values Are Equal
+
+ySubscriptYOffset
+Values Are Equal
+
+ySuperscriptXSize
+Values Are Equal
+
+ySuperscriptYSize
+Values Are Equal
+
+ySuperscriptXOffset
+Values Are Equal
+
+ySuperscriptYOffset
+Values Are Equal
+
+yStrikeoutSize
+['120', '50']
+yStrikeoutPosition
+Values Are Equal
+
+sFamilyClass
+Values Are Equal
+
+ulUnicodeRange1
+['3758107391', '3758097151']
+ulUnicodeRange2
+['3489701979', '1342208251']
+ulUnicodeRange3
+['8', '0']
+ulUnicodeRange4
+Values Are Equal
+
+achVendID
+Values Are Equal
+
+fsSelection
+Values Are Equal
+
+fsFirstCharIndex
+Values Are Equal
+
+fsLastCharIndex
+['65276', '65533']
+sTypoAscender
+['776', '693']
+sTypoDescender
+['-188', '-168']
+sTypoLineGap
+['61', '164']
+usWinAscent
+['1274', '835']
+usWinDescent
+['551', '259']
+
+```
+---
+If you want to use `diff`, generate the metrics files one by one.
+
+```
+python OS2_info.py -i 'font1.ttf' >> font1.metrics
+python OS2_info.py -i 'font2.ttf' >> font2.metrics
+
+```
+---
+
+You can also retrieve individual OS/2 properties:
+
+```
+python OS2_info.py -i 'font1.ttf' -p fsLastCharIndex
+```
